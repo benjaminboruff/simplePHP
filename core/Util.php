4f3e9af8e66a6b6ad84732cd9aa8dd8256e2f697
Benjamin H Boruff <ben@boruff.me>
2018-10-06 16:39:17 -0400

    added autoloading of all classes - forgot to add new files :)

---
+++ core/Util.php
@@ -0,0 +1,13 @@
+<?php
+
+class Util
+{
+    // dump and die
+    public static function dd($data)
+    {
+        //echo "<pre>";
+        var_dump($data);
+        //echo "</pre>";
+        die();
+    }
+}
